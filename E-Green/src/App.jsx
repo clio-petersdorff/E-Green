f7a90dea43c11e2ccbfec299f8100b91155e9948
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-05-19 00:25:53 +0200

    Created functions for APIs

--- E-Green/src/App.jsx
+++ E-Green/src/App.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
 import Homepage from './components/Homepage'
 import RaiseAwareness from './components/RaiseAwareness'
+import axios from 'axios';
+import { co2 } from '@tgwf/co2';
 
 import '../src/App.css'
 
 export default function App() {
+  let myKeySpeed = "AIzaSyBbPGKJV_6KhpruB8Q2Ra15LdU95i0MWzA"
+
+  function getBytesNumber(url){
+
+    axios.get(`https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed`, {
+      params:{
+        url: url,
+        key:myKeySpeed
+      }
+    })
+    .then(function (response) {
+      let bytes = response.data.lighthouseResult.audits['total-byte-weight'].numericValue;
+      console.log(bytes);
+      getHostName(url,bytes);
+      // getCO2gramsPerView(bytes);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    }
+
+  
+
+    function getHostName(url,bytes){
+      const parsedUrl = new URL(url); // Parse the URL
+      axios.get(`https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_fcNZEBwlbIhvWR5qKGQP1v0MJibor&domainName=${url}`,{
+        params:{
+          outputFormat:"JSON"
+        }
+      })
+      .then((response)=>{
+          let hosterName = response.data.WhoisRecord.registrarName;
+          console.log(hosterName);
+          getIsGreenHosting(hosterName,bytes)
+      })
+      .catch(function(error){
+          console.log(error);
+      });
+  }
+
+  function getIsGreenHosting(hosterName,bytes){
+    axios.get(`https://api.thegreenwebfoundation.org/api/v3/greencheck/${hosterName}`)
+    .then((response)=>{
+      let isGreen = response.data.green;
+      console.log(response.data.green);
+      getCO2gramsPerView(bytes,isGreen);
+    })
+    .catch(function(error){
+      console.log(error);
+  });
+  }
+
+
+  function getCO2gramsPerView(bytes,isGreen){
+    const swd = new co2({model: "swd"});
+
+    console.log(swd.perVisit(bytes,isGreen));
+  }
+
+  // getBytesNumber('https://orlanebdesign.com');
+
   return (
     <div>
       < Homepage />
--- E-Green/src/components/Form.jsx
+++ E-Green/src/components/Form.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
+  const [url,setUrl] = useState("");
+  const [validUrl,setValidUrl] = useState(true);
+
+  function handleChange(event){
+    setUrl(event.target.value);
+  }
+
+  function checkIfValidUrl(url){
+    try {
+      let result = Boolean(new URL(url));
+      setValidUrl(result);
+      return result;
+    }
+    catch(e){
+      setValidUrl(false);
+      return false;
+    }
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    if(checkIfValidUrl(url)){
+      //Cb function
+      console.log("good");
+    }
+
+    console.log('bad');
+
+    setUrl("");
+  }
+
+
   return (
-        <form>
-            <input placeholder='Enter your URL' />
+        <form onSubmit={(event)=>handleSubmit(event)}>
+            <input placeholder='Enter your URL' value={url} onChange={(event)=>handleChange(event)} />
             <button>Submit</button>
+
+            {!validUrl && <p>Try again, this isn't a valid URL</p>}
 
             <button className='quizOnlyButton'>Take the quiz only</button>
         </form>
